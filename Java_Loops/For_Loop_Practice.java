fd92d7a68274d60336ce01ce8256c3edf5255058
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-28 14:35:29 +0530

    Add Java for loop basic practice program

---
+++ Java_Loops/For_Loop_Practice.java
@@ -0,0 +1,10 @@
+public class For_Loop_Practice {
+    public static void main(String[] args) {
+        
+        // Declare 'i' as an int, start at 1, and go up to/including 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        
+    }
+}
